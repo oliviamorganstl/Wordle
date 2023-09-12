0849de6f835899fd045a39ed88457019b688549f
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-09-11 19:59:14 -0600

    Selected random word + displayed it on first row

--- Wordle.py
+++ Wordle.py
@@ -13,11 +13,25 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
+    def enter_action(s): #I think this method is for user actions
+        #gw.show_message("You have to implement this method.") commented out for now
+        pass
+
+    def select_random_word(word_list): #Brings in the 5 letter words as a parameter then picks one.
+        random_word = random.choice(word_list)
+        return random_word
+
+
+    selected_word = select_random_word(FIVE_LETTER_WORDS)
+    print("Randomly selected word:", selected_word) #Prints the five letter word in the console, not necessary for production but helping for testing.
+
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+    for i in range(len(selected_word)): #i is the column number
+        letter = selected_word[i]
+        gw.set_square_letter(0, i, letter) #we are assuming row 0 in the parameter
 
 # Startup code
 
